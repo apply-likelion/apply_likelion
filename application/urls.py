5b8c9ac42c3441942cda2246bcbeb96a91894f89
Sinhaecheol <since19990820@gmail.com>
2019-12-02 01:56:29 +0900

    app_url bugfix

--- application/urls.py
+++ application/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'application'
 urlpatterns = [
     path('app/', views.application, name='application'),
-    # path('/create/', views.create, name='create'),
-    path('asd', views.application, name='application'),
-    path('/newapplication/', views.applicationform, name='newapplication'),
-    path('update/<int:pk>', views.update, name='update'),  
+    # path('create/', views.create, name='create'),
+    path('newapplication/', views.applicationform, name='newapplication'),
+    path('update/<int:pk>/', views.update, name='update'),  
 ]
